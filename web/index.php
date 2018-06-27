a581d490dad252654e4cccddbb0eb582cd7dc8e4
Anoop S <anoop.santhanam@gmail.com>
2018-06-27 12:15:22 +0530

    Fixing cors

--- web/index.php
+++ web/index.php
@@ -43,7 +43,7 @@ $app->register(new Silex\Provider\SessionServiceProvider, array(
     'session.storage.save_path' => dirname(__DIR__) . '/tmp/sessions'
 ));
 
-$app->register(new TH\Silex\CORS\CORSProvider);
+// $app->register(new TH\Silex\CORS\CORSProvider);
 
 $app->before(function(Request $request) use($app){
     $request->getSession()->start();
