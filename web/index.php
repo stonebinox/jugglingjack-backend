4445e9238e0eae8735a9d31b8a009abf868d2f1b
Anoop S <anoop.santhanam@gmail.com>
2018-07-03 13:17:16 +0530

    Fixing signup

--- web/index.php
+++ web/index.php
@@ -73,11 +73,13 @@ $app->post("/api/signup", function(Request $request) use($app){
             if ($request->get("admin_id") == 2) {
                 $company = new companyMaster;
                 $r2 = $company->createCompany($request->get("company"), $request->get("company_description"));
+                echo $r2.'<br>';
                 if (is_numeric($r2)) {
                     $companyMember = new companyMemberMaster;
                     $e = explode("ACCOUNT_CREATED_", $response);
                     $userID = $e[1];
                     $r2 = $companyMember->addCompanyMember($r2, $userID);
+                    echo $r2.'<br>';
                     if ($r2 != "COMPANY_MEMBER_ADDED") {
                         return $r2;
                     }
